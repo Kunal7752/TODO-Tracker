80851ce1dd06fe9b856cdcad6d9abf91049641c0
Kunal Deshpande <kunaldeshpande97@gmail.com>
2025-06-07 15:15:18 -0400

    Style changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,12 @@ import { connectDB } from './lib/db.js';
 import taskRoutes from './routes/task.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
+import path from 'path';
 
 const app = express();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -22,6 +24,14 @@ app.use(cookieParser());
 
 app.use('/api/auth/', authRoutes);
 app.use('/api/tasks/', taskRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
     console.log("Server is running on port 5001");
